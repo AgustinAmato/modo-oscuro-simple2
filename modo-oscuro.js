5a15b6eab23bb6a67a753b5053c9b5ef01434ab4
agustinamato@outlook.com <agustinamato@outlook.com>
2023-07-24 10:04:48 -0300

    modo oscuro

---
+++ modo-oscuro.js
@@ -0,0 +1,49 @@
+
+let toggle = document.getElementById('container');
+let body =document.querySelector('body');
+
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+    body.classList.toggle('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
